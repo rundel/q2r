c35aad687796f1a85b26f8edf710b295fe9a0e8b
Colin Rundel <rundel@gmail.com>
2026-05-11 15:09:03 -0400

    Add 2026-05-07-new-blog-home round-trip test cases

--- tests/testthat/test-quarto-web-pd-rt.R
+++ tests/testthat/test-quarto-web-pd-rt.R
@@ -1595,6 +1595,17 @@ test_that("docs/blog/posts/2026-05-05-quarto-2-parsing/index.qmd", {
   expect_pd_ast_equal(pd2, pd)
 })
 
+test_that("docs/blog/posts/2026-05-07-new-blog-home/index.qmd", {
+  skip_if_no_quarto_web()
+  text = quarto_web_read("docs/blog/posts/2026-05-07-new-blog-home/index.qmd")
+  pd = pampa_parse_pd(text, quiet = TRUE)
+  if (has_error_diagnostics(pd)) skip("initial parse produced error diagnostics")
+  rendered = to_qmd(pd)
+  pd2 = pampa_parse_pd(rendered, quiet = TRUE)
+  expect_no_error_diagnostics(pd2)
+  expect_pd_ast_equal(pd2, pd)
+})
+
 test_that("docs/books/_book-vs-website-key.qmd", {
   skip_if_no_quarto_web()
   text = quarto_web_read("docs/books/_book-vs-website-key.qmd")
--- tests/testthat/test-quarto-web-ts-rt.R
+++ tests/testthat/test-quarto-web-ts-rt.R
@@ -1595,6 +1595,17 @@ test_that("docs/blog/posts/2026-05-05-quarto-2-parsing/index.qmd", {
   expect_ts_ast_equal(ts2, ts)
 })
 
+test_that("docs/blog/posts/2026-05-07-new-blog-home/index.qmd", {
+  skip_if_no_quarto_web()
+  text = quarto_web_read("docs/blog/posts/2026-05-07-new-blog-home/index.qmd")
+  ts = pampa_parse_ts(text, quiet = TRUE)
+  if (has_error_diagnostics(ts)) skip("initial parse produced error diagnostics")
+  rendered = to_qmd(ts)
+  ts2 = pampa_parse_ts(rendered, quiet = TRUE)
+  expect_no_error_diagnostics(ts2)
+  expect_ts_ast_equal(ts2, ts)
+})
+
 test_that("docs/books/_book-vs-website-key.qmd", {
   skip_if_no_quarto_web()
   text = quarto_web_read("docs/books/_book-vs-website-key.qmd")
